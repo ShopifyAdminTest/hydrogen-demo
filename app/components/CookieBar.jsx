50b08403031eff1c28450ee86af51306661806e6
Grigor Borisov <grishocska@gmail.com>
2023-12-07 14:38:52 +0200

    Change script source

--- app/components/CookieBar.jsx
+++ app/components/CookieBar.jsx
@@ -2,20 +2,20 @@ import React, { useEffect } from 'react';
 
 export default function CookieBar({ store, customer_id = 0, trackingConsent = () => {}}) {
     useEffect(() => {
-        window.iSenseAppSettings = {
+        window.consentmoAppSettings = {
             shop: store,
             customer_id: customer_id || 0,
             setTrackingConsent: (value) => {
             if (value) {
-                alert('valid consent');
+                console.log('valid consent');
             } else {
-                alert('invalid consent')
+                console.log('invalid consent')
             }
             }
         }
         }, [])
   
     return (
-       <script src="https://gdprcdn.b-cdn.net/js/gdpr_cookie_consent_headless.min.js"></script>
+       <script src="https://gdpr.apps.isenselabs.com/js/gdpr_cookie_consent_headless.original.js"></script>
     )
   }
